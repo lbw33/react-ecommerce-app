33286e31bb3158c460f7b89a051a0f0fbb2b025f
Luke Wickens <lukewickens1989@gmail.com>
2021-12-08 23:30:56 +0000

    added Router and useNavigate to menu-items

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+import { Routes, Route } from "react-router";
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/shop/hats" element={<HatsPage />} />
+      </Routes>
     </div>
   );
 }
