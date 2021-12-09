490a38a1e40df84ca571108a21e414173429fce9
Luke Wickens <lukewickens1989@gmail.com>
2021-12-09 10:30:27 +0000

    added shop collection preview and items w/ styling.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component.jsx";
 import { Routes, Route } from "react-router";
-
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-);
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/shop/hats" element={<HatsPage />} />
+        <Route path="/shop" element={<ShopPage />} />
       </Routes>
     </div>
   );
